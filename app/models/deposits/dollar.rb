565a3539fe15a19cf43ab7e0019b639e7e1ba618
wangdong <wangdong@xueqiu.com>
2018-01-29 16:53:57 +0800

    support dollar and renminbi trading

---
+++ app/models/deposits/dollar.rb
@@ -0,0 +1,16 @@
+module Deposits
+  class Dollar < ::Deposit
+    include ::AasmAbsolutely
+    include ::Deposits::Bankable
+
+    def charge!(txid)
+      with_lock do
+        submit!
+        accept!
+        touch(:done_at)
+        update_attribute(:txid, txid)
+      end
+    end
+
+  end
+end
